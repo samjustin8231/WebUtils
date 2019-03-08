47a3e728a66157c0f276ab9c5868f2b6bb2eb0aa
samjustin8231 <sam8231@163.com>
2019-03-08 16:11:08 +0800

    feat: add redis distributed lock demo;

--- src/main/java/com/example/webutils/common/distributedlock/RedisLockConfig.java
+++ src/main/java/com/example/webutils/common/distributedlock/RedisLockConfig.java
@@ -1,9 +1,30 @@
 package com.example.webutils.common.distributedlock;
 
+import com.crossoverjie.distributed.lock.redis.RedisLock;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import redis.clients.jedis.HostAndPort;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisCluster;
+
 /**
  * 类的实现描述：TODO 类实现描述
  *
  * @author sunyajun 2019/3/8 3:44 PM
  */
+@Configuration
 public class RedisLockConfig {
+
+	public static final String HOST = "127.0.0.1";
+	public static final int PORT = 6379;    //redis默认端口
+
+	@Bean
+	public RedisLock build() {
+		RedisLock redisLock = new RedisLock();
+		Jedis jedis = new Jedis(HOST, PORT);
+		// Jedis 或 JedisCluster 都可以
+		redisLock.setJedis(jedis);
+		return redisLock;
+	}
+
 }
--- src/main/java/com/example/webutils/common/distributedlock/RedisLockTest.java
+++ src/main/java/com/example/webutils/common/distributedlock/RedisLockTest.java
@@ -1,9 +1,63 @@
 package com.example.webutils.common.distributedlock;
 
+import com.crossoverjie.distributed.lock.redis.RedisLock;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
 /**
  * 类的实现描述：TODO 类实现描述
  *
  * @author sunyajun 2019/3/8 3:47 PM
  */
+@RestController
+@Slf4j
 public class RedisLockTest {
+	@Autowired
+	private RedisLock redisLock;
+
+	public void use(int index) {
+		String key = "key";
+		String request = UUID.randomUUID().toString();
+		try {
+			boolean locktest = redisLock.tryLock(key, request);
+			if (!locktest) {
+				log.info("index:{}, 线程：{}，获取分布式锁失败！", index, Thread.currentThread().getName());
+				return;
+			}
+
+			//do something
+			log.info("index:{}, 线程：{}，获取了分布式锁！", index, Thread.currentThread().getName());
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+		} catch (Exception e) {
+			log.error("try lock error:{}", e);
+		} finally {
+			log.info("index:{}, 线程：{}，释放了分布式锁！", index, Thread.currentThread().getName());
+			redisLock.unlock(key, request);
+		}
+
+	}
+
+	@RequestMapping("/redisLock")
+	public String redisLock() {
+		for (int i = 0; i < 10; i++) {
+			int finalI = i;
+			new Thread() {
+				@Override
+				public void run() {
+					use(finalI);
+				}
+			}.start();
+		}
+		return "redisLock";
+	}
+
 }
