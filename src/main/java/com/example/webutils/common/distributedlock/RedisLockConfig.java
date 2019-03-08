c2a3cde1391ea1af6c5c0c79cdda87011f66b35b
samjustin8231 <sam8231@163.com>
2019-03-08 16:10:43 +0800

    feat: add redis distributed lock demo;

---
+++ src/main/java/com/example/webutils/common/distributedlock/RedisLockConfig.java
@@ -0,0 +1,9 @@
+package com.example.webutils.common.distributedlock;
+
+/**
+ * 类的实现描述：TODO 类实现描述
+ *
+ * @author sunyajun 2019/3/8 3:44 PM
+ */
+public class RedisLockConfig {
+}
---
+++ src/main/java/com/example/webutils/common/distributedlock/RedisLockTest.java
@@ -0,0 +1,9 @@
+package com.example.webutils.common.distributedlock;
+
+/**
+ * 类的实现描述：TODO 类实现描述
+ *
+ * @author sunyajun 2019/3/8 3:47 PM
+ */
+public class RedisLockTest {
+}
